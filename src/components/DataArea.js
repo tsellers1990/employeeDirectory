b83daa63db1d9863b5ddbf18f6e5357f7293015a
Tymothy Sellers <tsellers1990@gmail.com>
2020-06-02 09:57:45 -0600

    finished the final componentsgit add .

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import DataTable from "./DataTable";
 import Nav from "./Nav";
 import API from "../utils/API";
-import "../styles/DataArea.css";
+// import "../styles/DataArea.css";
 
 export default class DataArea extends Component {
   state = {
